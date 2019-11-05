433893ae0faeffad06abe824979cd1d60fb8ca81
salmazz <salmamehanny@gmail.com>
2019-11-06 00:38:59 +0200

    add database

--- config/database.php
+++ config/database.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'host' => '127.0.0.1',
+    'host' => 'remotemysql.com',
     'port' => '3306',
-    'database' => 'shortly',
-    'username' => 'root',
-    'password' => '',
+    'database' => 'a1OztnspK8',
+    'username' => 'a1OztnspK8',
+    'password' => 'kWYrtUmjxa',
     'charset' => 'utf8mb4',
     'collation' => 'utf8mb4_unicode_ci',
 ];
